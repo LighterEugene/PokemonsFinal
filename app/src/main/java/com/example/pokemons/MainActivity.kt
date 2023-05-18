6200a2798e3348206ff1a94745a1f0a976f2155d
Lighter <agniage@list.ru>
2023-05-18 16:45:06 +0300

    f

--- app/src/main/java/com/example/pokemons/MainActivity.kt
+++ app/src/main/java/com/example/pokemons/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.pokemons
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {//Probe
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
